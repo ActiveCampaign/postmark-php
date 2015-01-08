8bc5ccff44faf38c9ace8ba9024970aa8570d907
Andrew Theken <theken.1@osu.edu>
2015-01-08 14:45:39 -0500

    Adding phpdoc.

--- src/Postmark/PostmarkAdminClient.php
+++ src/Postmark/PostmarkAdminClient.php
@@ -7,16 +7,14 @@ use Postmark\PostmarkClientBase as PostmarkClientBase;
 
 /**
  * The PostmarkAdminClient allows users to access and modify
- * "account-wide" settings. At this time the API supports
- * create, read, update, and delete operations are supported
- * for the "Sender Signatures", and "Servers."
+ * "Account-wide" settings. At this time the API supports
+ * management of the "Sender Signatures", and "Servers."
  */
 class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Create a new PostmarkAdminClient.
-	 * @param string $account_token is NOT the same as a Server Token -
-	 * you can get your account token from this page: https://postmarkapp.com/account/edit
+	 * @param string $account_token is NOT the same as a Server Token - you can get your account token from this page: https://postmarkapp.com/account/edit
 	 */
 	function __construct($account_token) {
 		parent::__construct($account_token, "X-Postmark-Account-Token");
@@ -24,7 +22,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Request a given server by ID.
-	 * @param  int
+	 * @param int $id The Id for the server you wish to retrieve.
 	 * @return DynamicResponseModel
 	 */
 	function getServer($id) {
@@ -33,9 +31,9 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Get a list of all servers configured on the account.
-	 * @param  integer
-	 * @param  integer
-	 * @param  string
+	 * @param  integer $count The number of servers to retrieve in the request, defaults to 100.
+	 * @param  integer $offset The number of servers to "skip" when paging through lists of servers.
+	 * @param  string $name
 	 * @return DynamicResponseModel
 	 */
 	function listServers($count = 100, $offset = 0, $name = NULL) {
@@ -50,7 +48,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	 * Delete a Server used for sending/receiving mail. NOTE: To protect your account, you'll need to
 	 * contact support and request that they enable this feature on your account before you can use this
 	 * client to delete Servers.
-	 * @param  integer
+	 * @param  integer $id The ID of the Server to delete.
 	 * @return DynamicResponseModel
 	 */
 	function deleteServer($id) {
@@ -61,17 +59,17 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	 * Modify an existing Server. Any parameters passed with NULL will be
 	 * ignored (their existing values will not be modified).
 	 * @param  integer $id is the Identity of the Server to modify.
-	 * @param  string
-	 * @param  string
-	 * @param  bool
-	 * @param  bool
-	 * @param  string
-	 * @param  string
-	 * @param  string
-	 * @param  bool
-	 * @param  bool
-	 * @param  string
-	 * @param  integer
+	 * @param  string $name
+	 * @param  string $color
+	 * @param  bool $rawEmailEnabled
+	 * @param  bool $smtpApiActivated
+	 * @param  string $inboundHookUrl
+	 * @param  string $bounceHookUrl
+	 * @param  string $openHookUrl
+	 * @param  bool $postFirstOpenOnly
+	 * @param  bool $trackOpens
+	 * @param  string $inboundDomain
+	 * @param  integer $inboundSpamThreshold
 	 * @return DynamicResponseModel
 	 */
 	function editServer($id, $name = NULL, $color = NULL,
@@ -100,17 +98,17 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	/**
 	 * Create a new Server. Any parameters passed with NULL will be
 	 * ignored (their default values will be used).
-	 * @param  string
-	 * @param  string $color (maybe any of the following: purple, blue, turqoise, green, red, yellow, grey)
-	 * @param  bool
-	 * @param  bool
-	 * @param  string
-	 * @param  string
-	 * @param  string
-	 * @param  bool
-	 * @param  bool
-	 * @param  string
-	 * @param  integer
+	 * @param  string $name
+	 * @param  string $color The color shown for the server in the Postmark web UI (may be any of the following: purple, blue, turqoise, green, red, yellow, grey)
+	 * @param  bool $rawEmailEnabled
+	 * @param  bool $smtpApiActivated
+	 * @param  string $inboundHookUrl
+	 * @param  string $bounceHookUrl
+	 * @param  string $openHookUrl
+	 * @param  bool $postFirstOpenOnly
+	 * @param  bool $trackOpens
+	 * @param  string $inboundDomain
+	 * @param  integer $inboundSpamThreshold Between 0 and 30.
 	 * @return DynamicResponseModel
 	 */
 	function createServer($name, $color = NULL,
@@ -136,8 +134,8 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Get a "page" of Sender Signatures.
-	 * @param  integer
-	 * @param  integer
+	 * @param  integer $count
+	 * @param  integer $offset
 	 * @return DynamicResponseModel
 	 */
 	function listSenderSignatures($count = 100, $offset = 0) {
@@ -151,7 +149,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Get information for a sepcific Sender Signature.
-	 * @param  integer
+	 * @param  integer $id The ID for the Sender Signature you wish to retrieve.
 	 * @return DynamicResponseModel
 	 */
 	function getSenderSignature($id) {
@@ -162,9 +160,9 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	 * Create a new Sender Signature for a given email address. Note that you will need to
 	 * "verify" this Sender Signature by following a link that will be emailed to the "fromEmail"
 	 * address specified when calling this method.
-	 * @param  string
-	 * @param  string
-	 * @param  string
+	 * @param  string $fromEmail
+	 * @param  string $name
+	 * @param  string $replyToEmail
 	 * @return DynamicResponseModel
 	 */
 	function createSenderSignature($fromEmail, $name, $replyToEmail = NULL) {
@@ -179,9 +177,9 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Alter the defaults for a Sender Signature.
-	 * @param  integer
-	 * @param  string
-	 * @param  string
+	 * @param  integer $id
+	 * @param  string $name
+	 * @param  string $replyToEmail
 	 * @return DynamicResponseModel
 	 */
 	function editSenderSignature($id, $name = NULL, $replyToEmail = NULL) {
@@ -195,7 +193,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 
 	/**
 	 * Delete a Sender Signature with the given ID.
-	 * @param  integer
+	 * @param  integer $id
 	 * @return DynamicResponseModel
 	 */
 	function deleteSenderSignature($id) {
@@ -205,7 +203,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	/**
 	 * Cause a new verification email to be sent for an existing (unverified) Sender Signature.
 	 * Sender Signatures require verification before they may be used to send email through the Postmark API.
-	 * @param  integer
+	 * @param  integer $id
 	 * @return DynamicResponseModel
 	 */
 	function resendSenderSignatureConfirmation($id) {
@@ -216,7 +214,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	 * Request that the Postmark API updates verify the SPF records associated
 	 * with the Sender Signature's email address's domain. Configuring SPF is not required to use
 	 * Postmark, but it is highly recommended, and can improve delivery rates.
-	 * @param  integer
+	 * @param  integer $id
 	 * @return DynamicResponseModel
 	 */
 	function verifySenderSignatureSPF($id) {
@@ -227,7 +225,7 @@ class PostmarkAdminClient extends PostmarkClientBase {
 	 * Cause a new DKIM key to be generated and associated with the Sender Signature. This key must be added
 	 * to your email domain's DNS records. Including DKIM is not required, but is recommended. For more information
 	 * on DKIM and its purpose, see http://www.dkim.org/
-	 * @param  integer
+	 * @param  integer $id
 	 * @return DynamicResponseModel
 	 */
 	function requestNewSenderSignatureDKIM($id) {
