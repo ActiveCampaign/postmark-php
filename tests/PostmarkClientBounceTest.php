27352d1e5a76011121af7c85c6609dd40d7f8ccd
Andrew Theken <theken.1@osu.edu>
2015-01-16 11:36:30 -0500

    Renaming test to correct name.

--- tests/PostmarkClientBounceTest.php
+++ tests/PostmarkClientBounceTest.php
@@ -8,11 +8,12 @@ use \Postmark\PostmarkClient;
 
 class PostmarkClientBounceTest extends PostmarkClientBaseTest {
 
-	function testClientCanSendBasicMessage() {
+	function testClientCanGetDeliveryStatistics() {
 		$tk = parent::$testKeys;
 		$client = new PostmarkClient($tk->READ_INBOUND_TEST_SERVER_TOKEN);
 
 		$stats = $client->getDeliveryStatistics();
+
 		$this->assertNotEmpty($stats, 'Stats from getDeliveryStatistics() should never be null or empty.');
 		$this->assertGreaterThan(0, $stats->InactiveMails, "The inactive mail count should be greater than zero.");
 	}
