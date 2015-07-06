a9827b99cc776703a3179b50eac905dc59043261
Andrew Theken <andrew@attr.io>
2015-07-06 16:36:07 -0400

    Adding stub for templating API tests.

---
+++ tests/PostmarkClientTemplatesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Postmark\Tests;
+
+require_once __DIR__ . "/PostmarkClientBaseTest.php";
+
+class PostmarkClientTemplatesTest extends PostmarkClientBaseTest {
+
+	//create
+
+	//edit
+
+	//list
+
+	//get
+
+	//delete
+
+	//validate
+
+	//send
+}
+?>
