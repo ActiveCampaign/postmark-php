0a0904db682166ffbb13d16a3fd0aed643a65c0c
Andrew Theken <andrew@attr.io>
2016-10-26 07:28:10 -0400

    Add case for when the API is unavailable (503)

--- src/Postmark/PostmarkClientBase.php
+++ src/Postmark/PostmarkClientBase.php
@@ -146,7 +146,6 @@ abstract class PostmarkClientBase {
 				$result = json_decode($response->getBody(), true);
 				break;
 			case 401:
-
 				$ex = new PostmarkException();
 				$ex->message = 'Unauthorized: Missing or incorrect API token in header. ' .
 				'Please verify that you used the correct token when you constructed your client.';
@@ -155,13 +154,10 @@ abstract class PostmarkClientBase {
 				break;
 			case 422:
 				$ex = new PostmarkException();
-
 				$body = json_decode($response->getBody(), true);
-
 				$ex->httpStatusCode = 422;
 				$ex->postmarkApiErrorCode = $body['ErrorCode'];
 				$ex->message = $body['Message'];
-
 				throw $ex;
 				break;
 			case 500:
@@ -170,6 +166,12 @@ abstract class PostmarkClientBase {
 				$ex->message = 'Internal Server Error: This is an issue with Postmark’s servers processing your request. ' .
 				'In most cases the message is lost during the process, ' .
 				'and Postmark is notified so that we can investigate the issue.';
+				throw $ex;
+				break;
+			case 503:
+				$ex = new PostmarkException();
+				$ex->httpStatusCode = 503;
+				$ex->message = 'The Postmark API is currently unavailable due to scheduled downtime. Please try your request again, later.';
 				throw $ex;
 				break;
 		}
