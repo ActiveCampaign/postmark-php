ca4578d96dc73992ac9580f8b603453d7bf71510
George Steel <george@net-glue.co.uk>
2022-02-17 11:51:49 +0000

    Import used functions and classes

--- src/Postmark/Models/PostmarkException.php
+++ src/Postmark/Models/PostmarkException.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Postmark\Models;
 
+use Exception;
+
 /**
- * The exception thrown when the Postmark Client recieves an error from the API.
+ * The exception thrown when the Postmark Client receives an error from the API.
  */
-class PostmarkException extends \Exception
+class PostmarkException extends Exception
 {
     var $message;
     var $httpStatusCode;
