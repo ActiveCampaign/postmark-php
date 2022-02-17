412c3e6b70629c9a14c833d7b8f6b5b45715a5ba
George Steel <george@net-glue.co.uk>
2022-02-17 13:32:33 +0000

    Unit test basic behaviour of all the webhook accessors

--- tests/Unit/Models/Webhooks/WebhookConfigurationTest.php
+++ tests/Unit/Models/Webhooks/WebhookConfigurationTest.php
@@ -41,4 +41,58 @@ class WebhookConfigurationTest extends TestCase
         assert($object instanceof WebhookConfiguration);
         self::assertFalse($object->getEnabled());
     }
+
+    public function testBounceSettings(): void
+    {
+        $bounce = new WebhookConfigurationBounceTrigger(false, true);
+        self::assertFalse($bounce->getEnabled());
+        self::assertTrue($bounce->getIncludeContent());
+        $bounce = new WebhookConfigurationBounceTrigger(true, false);
+        self::assertTrue($bounce->getEnabled());
+        self::assertFalse($bounce->getIncludeContent());
+    }
+
+    public function testClickSettings(): void
+    {
+        $click = new WebhookConfigurationClickTrigger(true);
+        self::assertTrue($click->getEnabled());
+        $click = new WebhookConfigurationClickTrigger(false);
+        self::assertFalse($click->getEnabled());
+    }
+
+    public function testDeliverySettings(): void
+    {
+        $delivery = new WebhookConfigurationDeliveryTrigger(true);
+        self::assertTrue($delivery->getEnabled());
+        $delivery = new WebhookConfigurationDeliveryTrigger(false);
+        self::assertFalse($delivery->getEnabled());
+    }
+
+    public function testOpenSettings(): void
+    {
+        $open = new WebhookConfigurationOpenTrigger(true, false);
+        self::assertTrue($open->getEnabled());
+        self::assertFalse($open->getPostFirstOpenOnly());
+        $open = new WebhookConfigurationOpenTrigger(false, true);
+        self::assertFalse($open->getEnabled());
+        self::assertTrue($open->getPostFirstOpenOnly());
+    }
+
+    public function testSpamComplaintSettings(): void
+    {
+        $spam = new WebhookConfigurationSpamComplaintTrigger(true, false);
+        self::assertTrue($spam->getEnabled());
+        self::assertFalse($spam->getIncludeContent());
+        $spam = new WebhookConfigurationSpamComplaintTrigger(false, true);
+        self::assertFalse($spam->getEnabled());
+        self::assertTrue($spam->getIncludeContent());
+    }
+
+    public function testSubscriptionChangeSettings(): void
+    {
+        $subChange = new WebhookConfigurationSubscriptionChange(true);
+        self::assertTrue($subChange->getEnabled());
+        $subChange = new WebhookConfigurationSubscriptionChange(false);
+        self::assertFalse($subChange->getEnabled());
+    }
 }
