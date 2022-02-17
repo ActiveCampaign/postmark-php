239ea2a1b9832f1e6490ea6d1f49540b6b9c1ff7
George Steel <george@net-glue.co.uk>
2022-02-17 15:54:30 +0000

    Move the base client to a subdirectory and add unit test
    - Sets the default api base uri with a constant
    - handles network errors and invalid requests triggered from the http client
    - Adds useful response fixtures for testing

---
+++ src/Postmark/Exception/InvalidRequestMethod.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Postmark\Exception;
+
+use InvalidArgumentException;
+
+use function sprintf;
+
+final class InvalidRequestMethod extends InvalidArgumentException implements PostmarkException
+{
+    public static function with(string $method): self
+    {
+        return new self(sprintf(
+            'The request method "%s" is invalid',
+            $method
+        ));
+    }
+}
