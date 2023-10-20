73a2bc99bc333cdcfa42dc89a0ccb22a51c0b68b
Eli Wood <ewood@PC26VM7WKL.local>
2023-10-20 14:45:20 -0400

    updated unit tests

--- tests/PostmarkAdminClientDataRemovalTest.php
+++ tests/PostmarkAdminClientDataRemovalTest.php
@@ -8,24 +8,11 @@ use Postmark\PostmarkAdminClient as PostmarkAdminClient;
 
 class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
 
-	static function tearDownAfterClass() {
-		$tk = parent::$testKeys;
-		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
-
-//		$servers = $client->listServers();
-//
-//		foreach ($servers->servers as $key => $value) {
-//			if (preg_match('/^test-php.+/', $value->name) > 0) {
-//				$client->deleteServer($value->id);
-//			}
-//		}
-	}
-
 	function testClientCanCreateRequest() {
 		$tk = parent::$testKeys;
 		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
-		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com, true");
+		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
 
 		$this->assertNotEmpty($drr->ID);
         $this->assertNotEmpty($drr->Status);
