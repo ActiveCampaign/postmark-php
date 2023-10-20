f90d6b63edf1a4695f9a3c33e0d55186df81b9d5
Eli Wood <ewood@PC26VM7WKL.local>
2023-10-20 13:14:58 -0400

    adding some testing

--- tests/PostmarkAdminClientDataRemovalTest.php
+++ tests/PostmarkAdminClientDataRemovalTest.php
@@ -6,29 +6,30 @@ require_once __DIR__ . "/PostmarkClientBaseTest.php";
 
 use Postmark\PostmarkAdminClient as PostmarkAdminClient;
 
-class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
+class PostmarkAdminClientServersTest extends PostmarkClientBaseTest {
+
+	static function tearDownAfterClass() {
+		$tk = parent::$testKeys;
+		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
+
+//		$servers = $client->listServers();
+//
+//		foreach ($servers->servers as $key => $value) {
+//			if (preg_match('/^test-php.+/', $value->name) > 0) {
+//				$client->deleteServer($value->id);
+//			}
+//		}
+	}
 
 	function testClientCanCreateRequest() {
 		$tk = parent::$testKeys;
 		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
-		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
+		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com, true");
 
 		$this->assertNotEmpty($drr->ID);
         $this->assertNotEmpty($drr->Status);
 	}
-
-    function testCheckDataRemovalRequest() {
-        $tk = parent::$testKeys;
-        $client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
-
-        $createRequest = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
-        $checkRequest = $client->getDataRemoval($createRequest->ID);
-
-        $this->assertNotEmpty($checkRequest->ID);
-        $this->assertNotEmpty($checkRequest->Status);
-        $this->assertEquals($checkRequest->ID, $createRequest->ID);
-    }
 }
 
 ?>
