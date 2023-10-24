a7e91bcd62176ee6dd7da9276c417925665ed2a1
Eli Wood <ewood@PC26VM7WKL.local>
2023-10-24 10:57:35 -0400

    added test case for checking status

--- tests/PostmarkAdminClientDataRemovalTest.php
+++ tests/PostmarkAdminClientDataRemovalTest.php
@@ -8,15 +8,28 @@ use Postmark\PostmarkAdminClient as PostmarkAdminClient;
 
 class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
 
+    private $testId;
+
 	function testClientCanCreateRequest() {
 		$tk = parent::$testKeys;
 		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
 		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
 
+        $this->testId = $drr->ID;
 		$this->assertNotEmpty($drr->ID);
         $this->assertNotEmpty($drr->Status);
 	}
+
+    function testCheckDataRemovalRequest() {
+        $tk = parent::$testKeys;
+        $client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
+
+        $drr = $client->getDataRemoval($this->testId);
+
+        $this->assertNotEmpty($drr->ID);
+        $this->assertNotEmpty($drr->Status);
+    }
 }
 
 ?>
