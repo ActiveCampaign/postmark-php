91322d8d730f3ef45adee60aaa2f354125b5035f
Eli Wood <ewood@PC26VM7WKL.local>
2023-10-25 15:22:54 -0400

    updated

--- tests/PostmarkAdminClientDataRemovalTest.php
+++ tests/PostmarkAdminClientDataRemovalTest.php
@@ -17,7 +17,7 @@ class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
 		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
 
         $this->testId = $drr->ID;
-        fwrite(STDERR, print_r("!!!!!!!!!!! ---------- This is the data removal request ID ". $drr->ID, TRUE));
+        fwrite(STDERR, print_r("!!!!!!!!!!! ---------- This is the data removal request ID ". $this->testId, TRUE));
 		$this->assertNotEmpty($drr->ID);
         $this->assertNotEmpty($drr->Status);
 	}
@@ -26,6 +26,7 @@ class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
         $tk = parent::$testKeys;
         $client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
+        fwrite(STDERR, print_r("Data removal request ID ". $this->testId . " use it to check the request.", TRUE));
         $drr = $client->getDataRemoval($this->testId);
 
         $this->assertNotEmpty($drr->ID);
