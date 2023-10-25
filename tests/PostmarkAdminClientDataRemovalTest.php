0120d68dc3d61fd25df858fd73898749265a242c
Eli Wood <ewood@PC26VM7WKL.local>
2023-10-25 15:38:33 -0400

    clean up tests and make them more independent

--- tests/PostmarkAdminClientDataRemovalTest.php
+++ tests/PostmarkAdminClientDataRemovalTest.php
@@ -8,16 +8,12 @@ use Postmark\PostmarkAdminClient as PostmarkAdminClient;
 
 class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
 
-    private $testId = 0;
-
 	function testClientCanCreateRequest() {
 		$tk = parent::$testKeys;
 		$client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
 		$drr = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
 
-        $this->testId = $drr->ID;
-        fwrite(STDERR, print_r("!!!!!!!!!!! ---------- This is the data removal request ID ". $this->testId, TRUE));
 		$this->assertNotEmpty($drr->ID);
         $this->assertNotEmpty($drr->Status);
 	}
@@ -26,11 +22,12 @@ class PostmarkAdminClientDataRemovalTest extends PostmarkClientBaseTest {
         $tk = parent::$testKeys;
         $client = new PostmarkAdminClient($tk->WRITE_ACCOUNT_TOKEN, $tk->TEST_TIMEOUT);
 
-        fwrite(STDERR, print_r("Data removal request ID ". $this->testId . " use it to check the request.", TRUE));
-        $drr = $client->getDataRemoval($this->testId);
+        $createRequest = $client->createDataRemovalRequest("test@activecampaign.com", "test2@activecampaign.com", true);
+        $checkRequest = $client->getDataRemoval($createRequest->ID);
 
-        $this->assertNotEmpty($drr->ID);
-        $this->assertNotEmpty($drr->Status);
+        $this->assertNotEmpty($checkRequest->ID);
+        $this->assertNotEmpty($checkRequest->Status);
+        $this->assertEquals($checkRequest->ID, $createRequest->ID);
     }
 }
 
