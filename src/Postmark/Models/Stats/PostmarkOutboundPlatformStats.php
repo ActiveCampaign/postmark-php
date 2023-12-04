85cd3f08c38673917af1fdaf8e78358f46b4d4fc
Eli Wood <ewood@PC26VM7WKL.local>
2023-12-04 16:10:06 -0500

    removed duplicate model

--- src/Postmark/Models/Stats/PostmarkOutboundPlatformStats.php
+++ src/Postmark/Models/Stats/PostmarkOutboundPlatformStats.php
@@ -2,6 +2,8 @@
 
 namespace Postmark\Models\Stats;
 
+use Postmark\Models\Stats\DatedPlatformCount;
+
 class PostmarkOutboundPlatformStats
 {
     public array $Days; // DatedPlatformCount object
@@ -115,113 +117,3 @@ class PostmarkOutboundPlatformStats
         return $this;
     }
 }
-
-class DatedPlatformCount
-{
-    public string $Date;
-    public int $Desktop;
-    public int $Mobile;
-    public int $Webmail;
-    public int $Unknown;
-
-    public function __construct(array $values)
-    {
-        $this->Date = !empty($values['Date']) ? $values['Date'] : '';
-        $this->Desktop = !empty($values['Desktop']) ? $values['Desktop'] : 0;
-        $this->Mobile = !empty($values['Mobile']) ? $values['Mobile'] : 0;
-        $this->Webmail = !empty($values['Webmail']) ? $values['Webmail'] : 0;
-        $this->Unknown = !empty($values['Unknown']) ? $values['Unknown'] : 0;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDate(): string
-    {
-        return $this->Date;
-    }
-
-    /**
-     * @param string $Date
-     * @return DatedPlatformCount
-     */
-    public function setDate(string $Date): DatedPlatformCount
-    {
-        $this->Date = $Date;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDesktop(): int
-    {
-        return $this->Desktop;
-    }
-
-    /**
-     * @param int $Desktop
-     * @return DatedPlatformCount
-     */
-    public function setDesktop(int $Desktop): DatedPlatformCount
-    {
-        $this->Desktop = $Desktop;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getMobile(): int
-    {
-        return $this->Mobile;
-    }
-
-    /**
-     * @param int $Mobile
-     * @return DatedPlatformCount
-     */
-    public function setMobile(int $Mobile): DatedPlatformCount
-    {
-        $this->Mobile = $Mobile;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getWebmail(): int
-    {
-        return $this->Webmail;
-    }
-
-    /**
-     * @param int $Webmail
-     * @return DatedPlatformCount
-     */
-    public function setWebmail(int $Webmail): DatedPlatformCount
-    {
-        $this->Webmail = $Webmail;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getUnknown(): int
-    {
-        return $this->Unknown;
-    }
-
-    /**
-     * @param int $Unknown
-     * @return DatedPlatformCount
-     */
-    public function setUnknown(int $Unknown): DatedPlatformCount
-    {
-        $this->Unknown = $Unknown;
-        return $this;
-    }
-
-}
-
