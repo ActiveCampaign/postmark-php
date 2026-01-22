8cc5f816324b0ccd23635d0f1e2a9172a8809e45
Eli Wood <ewood@activecampaign.com>
2026-01-22 09:21:24 -0500

    type hints

--- src/Postmark/Models/PostmarkAttachment.php
+++ src/Postmark/Models/PostmarkAttachment.php
@@ -20,17 +20,17 @@ class PostmarkAttachment implements JsonSerializable
         $this->contentId = $contentId;
     }
 
-    public static function fromRawData($data, $attachmentName, $mimeType = null, $contentId = null)
+    public static function fromRawData(string $data, string $attachmentName, ?string $mimeType = null, ?string $contentId = null): PostmarkAttachment
     {
         return new PostmarkAttachment(base64_encode($data), $attachmentName, $mimeType, $contentId);
     }
 
-    public static function fromBase64EncodedData($base64EncodedData, $attachmentName, $mimeType = null, $contentId = null)
+    public static function fromBase64EncodedData(string $base64EncodedData, string $attachmentName, ?string $mimeType = null, ?string $contentId = null): PostmarkAttachment
     {
         return new PostmarkAttachment($base64EncodedData, $attachmentName, $mimeType, $contentId);
     }
 
-    public static function fromFile($filePath, $attachmentName, $mimeType = null, $contentId = null)
+    public static function fromFile(string $filePath, string $attachmentName, ?string $mimeType = null, ?string $contentId = null): PostmarkAttachment
     {
         return new PostmarkAttachment(base64_encode(file_get_contents($filePath)), $attachmentName, $mimeType, $contentId);
     }
